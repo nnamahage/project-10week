e4e31a4d6ec53871ea6fdc3e03c0936e6589c7b8
Abdu <aabdugalimov@mail.ru>
2022-02-23 23:57:40 +0600

    Added Main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-StudentManagementSystem student = new StudentManagementSystem();
-
+        StudentManagementSystem sms = new StudentManagementSystem();
+        sms.addStudent(1, "Abduakhit", "Abdugalimov", 19, 3.2);
+        sms.removeStudent(1);
+        TeacherManagement tm = new TeacherManagement();
+        tm.addTeacher(1, "Gulzhazira", "Omirzakova", "Biology");
+        tm.removeTeacher(1);
     }
 }
